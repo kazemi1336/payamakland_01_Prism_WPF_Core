0479a88f9e992dc8082eeb428b5f5742744d4024
persiamahak@gmail.com <persiamahak@gmail.com>
2020-11-13 11:56:51 +0330

    1st_Commit

--- payamakland_WPF_Core/ViewModels/MainWindowViewModel.cs
+++ payamakland_WPF_Core/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,17 @@
 ﻿using Prism.Mvvm;
+using Prism.Commands;
+using System;
+using Prism.Regions;
 
 namespace payamakland_WPF_Core.ViewModels
 {
+    //private readonly IRegionManager _regionManager;
+
     public class MainWindowViewModel : BindableBase
     {
-
+        public DelegateCommand GotoComboBoxCommand { get; set; }
+        public DelegateCommand GotoListBoxCommand { get; set; }
+        public DelegateCommand GotoTabControlCommand { get; set; }
         private string _title = "Prism Application";
         public string Title
         {
@@ -12,9 +19,28 @@ namespace payamakland_WPF_Core.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        public MainWindowViewModel()
+        IRegionManager _regionManager;
+        public MainWindowViewModel(IRegionManager regionManager)
         {
+            _regionManager = regionManager;
+            GotoComboBoxCommand = new DelegateCommand(GotoComboBox);
+            GotoListBoxCommand = new DelegateCommand(GotoListBox);
+            GotoTabControlCommand = new DelegateCommand(GotoTabControl);
+        }
 
+        private void GotoTabControl()
+        {
+            _regionManager.RequestNavigate("ContentRegion", "TabControl");
+        }
+
+        private void GotoListBox()
+        {
+            _regionManager.RequestNavigate("ContentRegion", "ListBox");
+        }
+
+        private void GotoComboBox()
+        {
+            _regionManager.RequestNavigate("ContentRegion", "ComboBox");
         }
     }
 }
